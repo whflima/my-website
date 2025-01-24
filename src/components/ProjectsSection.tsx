4dc52070737ef5bdff0c01580ae433dd43cd7a6f
Welisson Hudson Fernandes Lima <wellissonsacer2000@gmail.com>
2025-01-23 21:55:11 -0300

    feat: added projects section.

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,15 @@
+
+export default function ProjectsSection() {
+  return (
+    <section id="projects" className="py-10 md:py-16">
+      <div className="container max-w-screen-xl mx-auto px-4">
+        <div className="flex flex-col lg:flex-row justify-between">
+          <div className="mb-10 lg:mb-0">
+            <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Projects</h1>
+            <p className="font-normal text-gray-500 text-xs md:text-base">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit exercitationem sequi, iure </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import EducationSection from "@/components/EducationSection";
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection";
 
 export default function Home() {
   return (
     <main className="flex-auto">
       <Header />
       <HomeSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <EducationSection />
     </main>
   );
 }
