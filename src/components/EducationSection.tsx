3af940e99df388f73f8b4f6487a250244cde9b2a
Welisson Hudson Fernandes Lima <wellissonsacer2000@gmail.com>
2025-03-03 22:47:31 -0300

    feat: add education section internationalization expressions.

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -1,49 +1,48 @@
+import { useTranslation } from "react-i18next";
+
 export default function EducationSection() {
+  const { t } = useTranslation();
   return (
     <section id="education" className="py-10 md:py-16">
       <div className="container max-w-screen-xl mx-auto px-4">
         <div className="mb-10">
           <h1 className="font-medium text-gray-700 dark:text-white text-3xl md:text-4xl mb-5">
-            Education
+            {t("header.menu.items-education")}
           </h1>
           <p className="font-normal text-gray-500 dark:text-gray-400 text-xs md:text-base text-justify">
-            I hold a strong educational background that blends theory and
-            practical skills, providing a solid foundation for tackling
-            real-world challenges.
+            {t("education.section-short_description")}
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
           <div className="bg-gray-50 dark:bg-gray-800 px-8 py-10 rounded-md">
             <div className="flex flex-col sm:flex-row justify-between font-medium text-gray-700 dark:text-white text-lg mb-4">
-              <p>Bachelor of Computer Science</p>
-              <p className="text-gray-500 dark:text-gray-300">2019 - 2023</p>
+              <p>{t("education.section.title-bachelor")}</p>
+              <p className="text-gray-500 dark:text-gray-300">
+                {t("education.section.date-bachelor")}
+              </p>
             </div>
             <p className="font-normal text-gray-500 dark:text-gray-400 text-md mb-4 text-justify">
-              A degree focused on programming, algorithms, data structures, and
-              system design. Developed strong problem-solving skills and
-              hands-on experience with software development and computer
-              systems.
+              {t("education.section.description-bachelor")}
             </p>
             <div className="relative">
               <h6 className="font-semibold text-gray-500 dark:text-gray-300 text-md relative z-10">
-                Average Performance: 8.21/10.0
+                {t("education.section.average_performance-bachelor")}
               </h6>
             </div>
           </div>
           <div className="bg-gray-50 dark:bg-gray-800 px-8 py-10 rounded-md">
             <div className="flex flex-col sm:flex-row justify-between font-medium text-gray-700 dark:text-white text-lg mb-4">
-              <p>IT Technician Course</p>
-              <p className="text-gray-500 dark:text-gray-300">2016 - Incomplete</p>
+              <p>{t("education.section.title-technician")}</p>
+              <p className="text-gray-500 dark:text-gray-300">
+                {t("education.section.date-technician")}
+              </p>
             </div>
             <p className="font-normal text-gray-500 dark:text-gray-400 text-md mb-4 text-justify">
-              A course focused on hardware troubleshooting, network setup, and
-              system maintenance. Gained hands-on experience in diagnosing
-              technical issues and providing IT support across different
-              environments.
+              {t("education.section.description-technician")}
             </p>
             <div className="relative">
               <h6 className="font-semibold text-gray-500 dark:text-gray-300 text-md relative z-10">
-                Average Performance: -
+                {t("education.section.average_performance-technician")}
               </h6>
             </div>
           </div>
